1924a1693d7bbc37b1735d94b75a5ddf9bb9585e
admin <admin@admin-PC>
2013-07-16 21:34:34 +0800

    wiki page motto extraction done

--- src/Wikipeida/WikiExtractor.java
+++ src/Wikipeida/WikiExtractor.java
@@ -21,9 +21,9 @@ import org.jsoup.select.Elements;
  * @author lyf
  */
 public class WikiExtractor {
-    public static String path = "";//../Input/";
+    public static String path = "../Input/";
     
-    public static void extractTable(String file) throws IOException{
+    public static void extractTable(String file, String univName) throws IOException{
         String html = FileUtils.readFileToString(new File(file));
         Document doc = Jsoup.parse(html);
         Elements tables = doc.select("table");
@@ -39,8 +39,18 @@ public class WikiExtractor {
                     break;
                 }
             }
+            if (motto.equals("---------------------")) {
+                for (Element tr : trs) {
+                    String th = tr.select("th").text();
+                    String td = tr.select("td").text();
+                    if (th.equals("Motto")) {
+                        motto = td;
+                        break;
+                    }
+                }
+            }
         }
-        print(motto);
+        print(univName+": "+motto);
     }
     
     public static void print(String text){
@@ -67,17 +77,18 @@ public class WikiExtractor {
         return null;
     }
     
-    public static void main(String[] args) {
+    public static void download(){
         List<String> lines = new ArrayList<String>();
         try {
             lines = FileUtils.readLines(new File(path+"Elite96.txt"));
         } catch (IOException ex) {
             Logger.getLogger(WikiExtractor.class.getName()).log(Level.SEVERE, null, ex);
         }
-        int iter = 1;
+        int iter = 0;
         while(true) {
             int failure = 0;
             int success = 0;
+            iter++;
             for (int i = 0; i < lines.size(); i++) {
                 if (new File(path+"WikiPages/" + (i + 1) + ".txt").exists()) continue;
                 try {
@@ -87,9 +98,9 @@ public class WikiExtractor {
                         urlPart = urlPart.replaceAll("&", "%26");
                     }
                     String wikiURL = "http://en.wikipedia.org/wiki/" + urlPart;
-                    //delay(3);
+                    delay(3);
                     print("Downloading " + (i + 1) + "...");
-                    Document doc = Jsoup.connect(wikiURL).userAgent("Chrome").timeout(0).get();
+                    Document doc = Jsoup.connect(wikiURL).userAgent("crawler4j (http://code.google.com/p/crawler4j/)").timeout(3000).get();
                     FileUtils.writeStringToFile(new File(path+"WikiPages/" + (i + 1) + ".txt"), doc.html());
                     success++;
                 } catch (IOException ex) {
@@ -100,6 +111,19 @@ public class WikiExtractor {
             }
             print("Iteration "+iter+": "+success+" succeeded, "+failure+" remaining...");
             if (failure == 0) break;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        List<String> lines = new ArrayList<String>();
+        try {
+            lines = FileUtils.readLines(new File(path+"Elite96.txt"));
+        } catch (IOException ex) {
+            Logger.getLogger(WikiExtractor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        for (int i=0; i<lines.size(); i++) {
+            String univName = lines.get(i).split("==")[0];
+            extractTable(path+"WikiPages/" + (i + 1) + ".txt", univName);
         }
     }
 }
